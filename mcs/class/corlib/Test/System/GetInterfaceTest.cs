b096c0922238323b6ce227314b2c2b2417ed6a0a
KonH <konh@yandex.ru>
2018-03-07 21:47:55 +0700

    Temp test file;

---
+++ mcs/class/corlib/Test/System/GetInterfaceTest.cs
@@ -0,0 +1,21 @@
+// GetInterfaceTest.cs - NUnit Test Cases for https://github.com/mono/mono/issues/6579
+//
+// Konstantin Khitrykh (konh@yandex.ru)
+//
+// (C) Konstantin Khitrykh
+// 
+
+using NUnit.Framework;
+using System;
+
+namespace MonoTests.System {
+	[TestFixture]
+	public class GetInterfaceTest {
+
+		// temporary
+		[Test]
+		public void GetInterfaceTemp() {
+			Assert.IsTrue(false);
+		}
+	}
+}
